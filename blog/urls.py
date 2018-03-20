981a6dc7b9043e476f85ef3b28b8852b91155ff8
laughingsolo <laughingsolo@laughingsolodeMacBook-Pro.local>
2018-03-20 09:15:23 +0800

    blog

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    url(r'^$',  views.IndexView.as_view(), name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archivesView.as_view(), name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category'),
+    url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
+    url(r'^about', views.about, name='about'),
+    url(r'^contact' ,views.contact, name='contact')
+
+
+]
